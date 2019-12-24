7efd4b79ffd0eed9bda56e9f83ea0cc7c6514d5e
unknown <abhisharmaec0982@gmail.com>
2019-12-24 20:54:02 +0100

    firstcommit

---
+++ LBS_MBLCompu_abhiproj/src/test/java/com/abhi/LocationBasedService/LocationBasedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.LocationBasedService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationBasedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
